76c560ec795b0554e99c40bae45f0fd243145d09
Prity Gupta <121803228+guptaprity@users.noreply.github.com>
2025-09-19 15:31:53 +0530

    Update Basics.sql

--- PL-SQL/Basics/Basics.sql
+++ PL-SQL/Basics/Basics.sql
@@ -42,3 +42,71 @@ SELECT MAX(SAL) INTO V_MAX FROM EMP;
 dbms_output.put_line(V_MAX);
 END;
 /
+
+
+--19-SEP-2025
+---1.Write a PL/SQL program to calculate the experience of the given employeem based on passed employee number ?
+
+--Enter value for empno: 7902
+--experience is:43years
+
+DECLARE 
+V_EMPNO NUMBER(4);
+v_HIRE DATE;
+EXP NUMBER(5);
+BEGIN 
+V_EMPNO := &EMPNO;
+SELECT HIREDATE INTO V_HIRE
+FROM EMP WHERE EMPNO=V_EMPNO;
+
+EXP:= TRUNC((SYSDATE-v_hire)/365);
+
+DBMS_OUTPUT.PUT_LINE('experience is: '|| EXP);
+
+END;
+/
+
+---OUTPUT---
+--Enter value for empno: 7844
+--experience is: 44
+
+
+--%TYPE
+--2.Write a PL/SQL program to retrieve and display the employee name, salary,
+ -- and hiredate from the EMP table for a user entered empno using 
+  --column level attributes(%type).
+DECLARE 
+V_empno emp.empno%type;
+v_ename emp.ename%type;
+v_sal emp.sal%type;
+v_hiredate emp.hiredate%type;
+begin
+v_empno:=&empno;
+select ename,sal,hiredate into v_ename, v_sal, v_hiredate from emp where empno = v_empno;
+dbms_output.put_line(v_ename||' '|| v_sal||' '||v_hiredate);
+end;
+/
+
+--%ROWTYPE
+--3.Write a PL/SQL program to display all the details of an employee from 
+ -- the EMP table for a user entered empno using row level attributes(%rowtype).
+DEClare
+V_EMPNO EMP.EMPNO%TYPE;
+R EMP%ROWTYPE;
+BEGIN
+V_EMPNO := &EMPNO;
+SELECT * INTO R FROM EMP
+WHERE EMPNO = V_EMPNO;
+DBMS_OUTPUT.PUT_LINE(R.ENAME || ' '||R.JOB||' '||R.SAL||' '||R.HIREDATE||' '||R.COMM);
+END;
+/
+
+
+
+
+
+
+
+
+
+
